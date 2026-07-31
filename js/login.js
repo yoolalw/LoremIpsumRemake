f41b9f69460ee8d5c9fb04be2793d5d460a2ca99
yoolalw <larakemilyprofissional@gmail.com>
2026-07-31 08:54:53 -0300

    feat: Criando arquivo json

---
+++ js/login.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn_login")
+const msg = document.getElementById("msg")
+msg.style.display = "none"
+
+btn.addEventListener("click", async (e) => {
+    e.preventDefault()
+
+    
+
+})
